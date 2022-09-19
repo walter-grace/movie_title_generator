fb7f65e78353ee66e008f5f8193c6c49700e70b6
Walter Grace <31444892+walter-grace@users.noreply.github.com>
2022-09-19 13:15:31 -0700

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: 'sk-5Bmp2hZkOXlmaLovA6h02J8amDHM0sntbUkyFERh',
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
