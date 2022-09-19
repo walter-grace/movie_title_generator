49a16f78c2a22ec803b77be112d9d84469636201
Walt Grace <nico.zahniser@gmail.com>
2022-09-19 13:08:33 -0700

    new

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: 'sk-5Bmp2hZkOXlmaLovA6h02J8amDHM0sntbUkyFERh',
 });
 const openai = new OpenAIApi(configuration);
 
